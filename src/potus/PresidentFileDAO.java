66137d307aef90641e1c599b1ec3086963f05fdc
Yvette Menase <yvette.j.menase@gmail.com>
2016-11-04 17:55:09 -0600

    created array

--- src/potus/PresidentFileDAO.java
+++ src/potus/PresidentFileDAO.java
@@ -18,14 +18,22 @@ public class PresidentFileDAO implements PresidentDAO {
 				String firstName = presidents[0];
 				String middleName = presidents[1];
 				String lastName = presidents[2];
-				int inaugYear = presidents[3];
-				int yearsInOffice = presidents[4];
-				int endYear = presidents[5];
-				int ageAtInaug = presidents[6];
+				int inaugYear = Integer.parseInt(presidents[3]);
+				double yearsInOffice = Double.parseDouble(presidents[4]);
+				int endYear = Integer.parseInt(presidents[5]);
+				int ageAtInaug = Integer.parseInt(presidents[6]);
 				String stateElected = presidents[7];
-				int electoralVotes = presidents[8];
-				int popularVotes = presidents[9];
-				int totalPopularVotes = presidents[10];
+				int electoralVotes = Integer.parseInt(presidents[8]);
+				int popularVotes = Integer.parseInt(presidents[9]);
+				int totalPopularVotes = Integer.parseInt(presidents[10]);
+				int totalElectoralVotes = Integer.parseInt(presidents[11]);
+				int ratingPoints = Integer.parseInt(presidents[12]);
+				String party = presidents[13];
+				String occupation = presidents[14];
+				String college = presidents[15];
+				double electoralPercentage = Double.parseDouble(presidents[16]);
+				double popularPercentage = Double.parseDouble(presidents[17]);
+				
 				
 				presidents.add(new President(symbol, name, price));
 			}
