67ecd7c083a8a1139caf4543c7ae097515d498bb
Yvette Menase <yvette.j.menase@gmail.com>
2016-11-04 17:14:33 -0600

    updating constructor and fields

--- src/potus/president.java
+++ src/potus/president.java
@@ -1,31 +1,49 @@
 package potus;
 
-public class president {
+public class President {
 	private String firstName;
 	private String middleName;
 	private String lastName;
-	private int startYear;
+	private int inaugYear;
+	private int yearsInOffice;
 	private int endYear;
+	private int ageAtInAug;
+	private String stateElected;
+	private int electoralVotes;
+	private int popularVotes;
+	private int totalPopularVotes;
+	private int totalElectoralVotes;
+	private int ratingPoints;
 	private String party;
-	/*
-	First	
-	Middle
-	Last
-	Years in office
-	Year first inaugurated	
-	Age at inauguration	
-	State elected from	
-	# of electoral votes	 
-	# of popular votes 	 
-	National total votes 	
-	Total electoral votes	
-	Rating points	
-	Political Party	
-	Occupation	
-	College	
-	% electoral	
-	% popular
-	*/
+	private String occupation;
+	private String college;
+	private int electoralPercentage;
+	private int popularPercentage;
+	
+
+	public President(String firstName, String middleName, String lastName, int inaugYear, int yearsInOffice,
+			int endYear, int ageAtInAug, String stateElected, int electoralVotes, int popularVotes,
+			int totalPopularVotes, int totalElectoralVotes, int ratingPoints, String party, String occupation,
+			String college, int electoralPercentage, int popularPercentage) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.inaugYear = inaugYear;
+		this.yearsInOffice = yearsInOffice;
+		this.endYear = endYear;
+		this.ageAtInAug = ageAtInAug;
+		this.stateElected = stateElected;
+		this.electoralVotes = electoralVotes;
+		this.popularVotes = popularVotes;
+		this.totalPopularVotes = totalPopularVotes;
+		this.totalElectoralVotes = totalElectoralVotes;
+		this.ratingPoints = ratingPoints;
+		this.party = party;
+		this.occupation = occupation;
+		this.college = college;
+		this.electoralPercentage = electoralPercentage;
+		this.popularPercentage = popularPercentage;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -44,12 +62,7 @@ public class president {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getStartYear() {
-		return startYear;
-	}
-	public void setStartYear(int startYear) {
-		this.startYear = startYear;
-	}
+
 	public int getEndYear() {
 		return endYear;
 	}
@@ -61,6 +74,84 @@ public class president {
 	}
 	public void setParty(String party) {
 		this.party = party;
+	}
+	public int getInaugYear() {
+		return inaugYear;
+	}
+	public void setInaugYear(int inaugYear) {
+		this.inaugYear = inaugYear;
+	}
+	public int getYearsInOffice() {
+		return yearsInOffice;
+	}
+	public void setYearsInOffice(int yearsInOffice) {
+		this.yearsInOffice = yearsInOffice;
+	}
+	public int getAgeAtInAug() {
+		return ageAtInAug;
+	}
+	public void setAgeAtInAug(int ageAtInAug) {
+		this.ageAtInAug = ageAtInAug;
+	}
+	public String getStateElected() {
+		return stateElected;
+	}
+	public void setStateElected(String stateElected) {
+		this.stateElected = stateElected;
+	}
+	public int getElectoralVotes() {
+		return electoralVotes;
+	}
+	public void setElectoralVotes(int electoralVotes) {
+		this.electoralVotes = electoralVotes;
+	}
+	public int getPopularVotes() {
+		return popularVotes;
+	}
+	public void setPopularVotes(int popularVotes) {
+		this.popularVotes = popularVotes;
+	}
+	public int getTotalPopularVotes() {
+		return totalPopularVotes;
+	}
+	public void setTotalPopularVotes(int totalPopularVotes) {
+		this.totalPopularVotes = totalPopularVotes;
+	}
+	public int getTotalElectoralVotes() {
+		return totalElectoralVotes;
+	}
+	public void setTotalElectoralVotes(int totalElectoralVotes) {
+		this.totalElectoralVotes = totalElectoralVotes;
+	}
+	public int getRatingPoints() {
+		return ratingPoints;
+	}
+	public void setRatingPoints(int ratingPoints) {
+		this.ratingPoints = ratingPoints;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public int getElectoralPercentage() {
+		return electoralPercentage;
+	}
+	public void setElectoralPercentage(int electoralPercentage) {
+		this.electoralPercentage = electoralPercentage;
+	}
+	public int getPopularPercentage() {
+		return popularPercentage;
+	}
+	public void setPopularPercentage(int popularPercentage) {
+		this.popularPercentage = popularPercentage;
 	}
 	
 }
