540263d74e114005460b2f72f18ecb151c7533e2
Yvette Menase <yvette.j.menase@gmail.com>
2016-11-04 16:51:12 -0600

    started a presidents class

--- src/potus/president.java
+++ src/potus/president.java
@@ -1,5 +1,49 @@
 package potus;
 
 public class president {
-
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private int startYear;
+	private int endYear;
+	private String party;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getStartYear() {
+		return startYear;
+	}
+	public void setStartYear(int startYear) {
+		this.startYear = startYear;
+	}
+	public int getEndYear() {
+		return endYear;
+	}
+	public void setEndYear(int endYear) {
+		this.endYear = endYear;
+	}
+	public String getParty() {
+		return party;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	
 }
