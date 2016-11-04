41de8e7def9720e216fc40fe20c588036c4b6c9c
Jonathan Edwards <edwardjd@colorado.edu>
2016-11-04 17:16:48 -0600

    servlet update

--- src/potus/PresidentServlet.java
+++ src/potus/PresidentServlet.java
@@ -14,21 +14,13 @@ public class PresidentServlet extends HttpServlet {
        
     
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String firstName = req.getParameter("firstName");
-		String middleName = req.getParameter("middleName");
-		String lastName = req.getParameter("lastName");
-		String startYear = req.getParameter("startYear");
-		String endYear = req.getParameter("endYear");
-		String party = req.getParameter("party");
+		President president = req.getParameter("President");
+		
 		
 		ServletContext context = getServletContext();
 		RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
-		req.setAttribute("firstName", firstName);
-		req.setAttribute("middleName", middleName);
-		req.setAttribute("lastName", lastName);
-		req.setAttribute("startYear", startYear);
-		req.setAttribute("endYear", endYear);
-		req.setAttribute("party", party);
+		req.setAttribute("President", president);
+		
 		dispatcher.forward(req, resp);
 	}
 
